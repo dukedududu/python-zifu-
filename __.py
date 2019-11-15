2d2bc04f623fa17aa29a91d7994dd08b6bc574f3
依凡 张 <2238406324@qq.com>
2019-11-15 19:33:48 +0800

    添加项目文件。

---
+++ __.py
@@ -0,0 +1,7 @@
+str='my name is jake'
+print(str*5)#输出五遍字符
+print(str[0:5])
+print(str[0:-1])#第一个字符到倒数第二个字符
+print(str+'hello world')
+print('hello world\n')
+print(r'hello world\n haha')#在字符串前面添加一个 r，表示原始字符串，不会发生转义
